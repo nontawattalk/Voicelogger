98e24a874a45a114dfd200e8c1c7b6c9cda63ee3
Mindtre <nontawatt@tunableproject.com>
2025-08-22 22:37:20 -0700

    Create __init__.py

---
+++ cli/__init__.py
@@ -0,0 +1,5 @@
+"""
+CLI package for voicelogger tool.
+This package exposes command-line interfaces for transcription, summarization,
+report generation, exporting transcripts in various formats, and encryption.
+"""
